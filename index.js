8118a823f1b73bc64f9d69ccc0f3b4e0a8edb3a6
Brad R. Allen <bradrallen@gmail.com>
2020-06-28 02:38:42 -0600

    finishing touches

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
+const app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -11,16 +12,16 @@ const pool = new Pool({
   }
 });
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/rate', (req, res) => res.render('pages/rate'))
-  .get('/db', async (req, res) => {
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.get('/rate', (req, res) => res.render('pages/rate',{rateMessage:""}))
+  app.get('/getrate', calculateRate)
+  app.get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM parcel');
+      const result = await client.query('SELECT * FROM letter');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
@@ -29,5 +30,46 @@ express()
       res.send("Error " + err);
     }
   })
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  app.get('/cool', (req, res) => res.send(cool()))
+
+  function calculateRate(req, res) {
+    var weight = req.query.weight;
+    console.log(weight);
+
+    var service = req.query.service;
+    console.log(service);
+
+    var table = req.query.service;
+    
+    if(table == "metered" || table == "stamped"){
+        var sql = "SELECT rate FROM letter WHERE weight = " + weight + "AND letterType = '" + service + "'";
+    } else {
+      var sql = "SELECT rate FROM " + table + " WHERE weight = " + weight;
+    }
+    console.log(sql);
+
+    pool.query(sql, function(err, result) {
+      // If an error occurred...
+      if (err) {
+        console.log("Error in query: ")
+                  console.log(err);
+                  res.end();
+      }
+ 
+    // Log this to the console for debugging purposes.
+    console.log("Back from DB with result:");
+    console.log(result.rows);
+
+    //res.setHeader('Content-Type', 'application/json');
+    //res.end(JSON.stringify(result.rows));
+    
+    var rateMessage = `The postage rate for a ${weight} ounce ${service} mailpiece is ${result.rows[0].rate}`;
+    res.render('pages/rate', {
+      rateMessage: rateMessage
+  });
+    
+    });   
+  }
+  
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
