08cd4d0dcfc4b93e9ee6f9d5cc2dff3c647a41a0
Brad R. Allen <bradrallen@gmail.com>
2020-06-27 23:31:14 -0600

    add styles and rate page

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || "postgres://mawrnubcqakjrq:82b6d3cd691b2cb9ef989d194ef68cf05759598c327c58ff129a4fa880df2ac9@ec2-35-169-254-43.compute-1.amazonaws.com:5432/dac2qujnnmo9h6",
   ssl: {
     rejectUnauthorized: false
   }
 });
-
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/rate', (req, res) => res.render('pages/rate'))
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
